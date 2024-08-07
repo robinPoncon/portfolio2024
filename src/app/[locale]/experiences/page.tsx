60cc241476895616ab0ccfd79ebffa894154e6d1
RobinP <poncon.robin@gmail.com>
2024-08-07 15:22:20 +0200

    add content experiences page

--- src/app/[locale]/experiences/page.tsx
+++ src/app/[locale]/experiences/page.tsx
@@ -8,12 +8,49 @@ const ExperiencePage = () => {
 			<h1 className="text-2xl text-center titleFont">Ses expériences</h1>
 			{/* Tableau style kanban pour afficher les expériences et formations */}
 			<ExperienceCard
-				title="Développeur front-end NextJS"
+				title="Développeur Front-End"
 				dates="Juillet 2023 à Aujourd'hui"
 				location="Auto-entrepreneur"
-				description="J’ai développé mon auto-entreprise dans le soin énergétique et la musique
-				thérapeutique. Je me suis formé et j’ai créé mon propre site internet avec NextJS
+				type="company"
+				technos="NextJS, React, TypeScript, Tailwind"
+				description="Il a développé son auto-entreprise dans le soin énergétique et la musique
+				thérapeutique. Il s'est formé et a créé son propre site internet avec NextJS
 				pour avoir un rendu moderne, intuitif et performant pour les utilisateurs."
+			/>
+			<p className="h-20 bg-customViolet w-2 mx-auto -my-10" />
+			<ExperienceCard
+				title="Développeur Fullstack"
+				dates="Février 2021 à Juillet 2023"
+				location="SOAN"
+				type="company"
+				technos="React, Symfony, Typescript, SCSS, jQuery, Javascript, Twig"
+				description="Il a commencé en tant alternant développeur front (Twig,
+				JS, jQuery, SCSS) puis il a évolué en CDI en tant que
+				développeur fullstack (React/Typescript et Symfony)."
+			/>
+			<p className="h-20 bg-customViolet w-2 mx-auto -my-10" />
+			<ExperienceCard
+				title="Développeur Web Fullstack"
+				dates="Décembre 2020 à Décembre 2021"
+				location="3W Academy"
+				type="school"
+				technos="React, Symfony, SCSS, jQuery, Javascript, Twig"
+				description="Il a commencé en tant alternant développeur front (Twig,
+				JS, jQuery, SCSS) puis il a évolué en CDI en tant que
+				développeur fullstack (React et Symfony)."
+			/>
+			<p className="h-20 bg-customViolet w-2 mx-auto -my-10" />
+			<ExperienceCard
+				title="Développeur Web Junior"
+				dates="Janvier 2019 à Juillet 2020"
+				location="Openclassrooms"
+				type="school"
+				technos="Symfony, PHP, Twig, JavaScript, jQuery, HTML5, CSS3"
+				description="Il a créé 5 projets de sites internet durant sa formation
+				en ligne. Intégration de maquette, création d'un site
+				wordpress, création d'un site dynamique en JS avec
+				intégration d'API, création d'un blog en PHP et JS, puis
+				une refonte d'un site d'un club de basket en Symfony."
 			/>
 		</section>
 	);
--- src/app/_components/ExperienceCard/ExperienceCard.tsx
+++ src/app/_components/ExperienceCard/ExperienceCard.tsx
@@ -8,31 +8,47 @@ type ExperienceCardProps = {
 	dates: string;
 	location: string;
 	description: string;
+	technos: string;
+	type: "company" | "school";
 };
 
-const ExperienceCard = ({ title, dates, location, description }: ExperienceCardProps) => {
+const ExperienceCard = ({
+	title,
+	dates,
+	location,
+	description,
+	technos,
+	type
+}: ExperienceCardProps) => {
 	const [descriptionDisplayed, setDescriptionDisplayed] = useState(false);
 
 	return (
-		<article className="border-4 rounded-md border-customViolet flex flex-col gap-3">
+		<article className="border-8 rounded-md border-customViolet flex flex-col gap-3 bg-lighterBg dark:bg-greyBg">
 			<p className="bg-customViolet py-1">
 				<Image
 					className="mx-auto"
-					alt="workcase icon"
-					src="/icons/work_icon.svg"
+					alt={type === "company" ? "workcase icon" : "certification icon"}
+					src={type === "company" ? "/icons/work_icon.svg" : "/icons/school_icon.svg"}
 					height={48}
 					width={48}
 				/>
 			</p>
 			<h2 className="text-xl px-3">{title}</h2>
-			<div className="flex gap-1 px-3">
-				<p className="font-semibold">Durée :</p>
-				<p className="italic">{dates}</p>
-			</div>
-			<div className="flex gap-1 px-3">
-				<p className="font-bold">Entreprise :</p>
-				<p className="italic">{location}</p>
-			</div>
+			<p className="px-3">
+				<span className="font-semibold">Durée :</span>
+				<span className="italic ml-1">{dates}</span>
+			</p>
+			<p className="px-3">
+				<span className="font-bold">
+					{type === "company" ? "Entreprise" : "Formation"} :
+				</span>
+				<span className="italic ml-1">{location}</span>
+			</p>
+			<p className="px-3">
+				<span className="font-bold">Technos :</span>
+				<span className="italic ml-1">{technos}</span>
+			</p>
+			{descriptionDisplayed && <p className="italic px-3 text-justify">{description}</p>}
 			<div className="mb-5">
 				<button
 					className="flex w-fit mx-auto bg-customViolet rounded-md text-white px-3 py-2"
@@ -50,7 +66,6 @@ const ExperienceCard = ({ title, dates, location, description }: ExperienceCardP
 						width={32}
 					/>
 				</button>
-				{descriptionDisplayed && <p className="italic mt-3 px-3 text-justify">{description}</p>}
 			</div>
 		</article>
 	);
